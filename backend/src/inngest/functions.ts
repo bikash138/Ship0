fc312ff59117d8af0ce751a4adfd70950ce8f990
Bikash Shaw <vshaw138@gmail.com>
2025-12-15 10:14:16 +0530

    added route handler for projects

--- backend/src/inngest/functions.ts
+++ backend/src/inngest/functions.ts
@@ -1,48 +1,18 @@
 import { Inngest } from "inngest";
-import { openai, createAgent, createTool } from "@inngest/agent-kit";
+import {
+  openai,
+  createAgent,
+  createTool,
+  createNetwork,
+} from "@inngest/agent-kit";
 import { Sandbox } from "@e2b/code-interpreter";
 import { z } from "zod";
+import { lastAssistantTextMessageContent } from "./utils";
+import { PROMPT } from "./prompt";
+import { prisma } from "../lib/prisma";
+import { MessageRole, MessageType } from "../../generated/prisma/enums";
 
 export const inngest = new Inngest({ id: "ship0" });
-
-const helloWord = inngest.createFunction(
-  { id: "hello-world" },
-  { event: "test/hello.world" },
-  async ({ event, step }) => {
-    const sandboxId = await step.run("get-sandbox-id", async () => {
-      const sandbox = await Sandbox.create("ship0-nextjs");
-      return sandbox.sandboxId;
-    });
-
-    // const supportAgent = createAgent({
-    //   model: openai({ model: "gpt-3.5-turbo" }),
-    //   name: "Typescript Agent",
-    //   system: `You are an autonomous software agent responsible for generating complete project structures that can be executed inside an E2B sandbox. Your output must be a pure JSON object describing all files required for the project. Each file must contain its full path (from the project root) and its exact content.
-    //         Rules:
-    //         - Output ONLY JSON. No markdown, no code fences, no comments, no explanations.
-    //         - Use this exact structure: { "files": [ { "path": "...", "content": "..." } ] }
-    //         - Include ALL necessary project files: configs, tsconfig, package.json, source code, components, styles, utilities, etc.
-    //         - The returned files must be self-contained and runnable inside a fresh E2B environment.
-    //         - File paths must use POSIX format (e.g., "src/app/page.tsx").
-    //         - Do not abbreviate or omit content. Return full file contents.
-    //         - Never include placeholders like "...". Always return complete code.
-    //         - If the project requires installing dependencies, include them inside package.json.`,
-    // });
-
-    // const { output } = await supportAgent.run("Create a todo app");
-
-    const sandboxUrl = await step.run("get-sandbox-url", async () => {
-      const sandboxUrl = await Sandbox.connect(sandboxId);
-      const host = sandboxUrl.getHost(3000);
-      return `http://${host}`;
-    });
-
-    return {
-      message: `The Sandbox URL is: ${sandboxUrl}`,
-    };
-  }
-);
-console.log("Functions created success");
 
 const codeAgentFunction = inngest.createFunction(
   { id: "code-agent" },
@@ -50,14 +20,15 @@ const codeAgentFunction = inngest.createFunction(
   //So the first task is to create the sandbox and connect with it with the id where the agent will code
   async ({ event, step }) => {
     const sandboxId = await step.run("get-sandbox-id", async () => {
-      const sandbox = await Sandbox.create("ship-nextjs");
+      const sandbox = await Sandbox.create("ship0-nextjs");
       return sandbox.sandboxId;
     });
+    3;
 
     const codeAgent = createAgent({
       name: "Code Agent",
       description: "Code Agent Expert in genrating frontend codes",
-      system: "",
+      system: PROMPT,
       model: openai({
         model: "gpt-4.1",
         defaultParameters: {
@@ -112,55 +83,121 @@ const codeAgentFunction = inngest.createFunction(
                 path: z.string(),
                 content: z.string(),
               })
-            )
+            ),
           }),
-          handler: async({files}, {step, network})=>{
-            const newFiles = await step?.run('createOrUpdateFiles', async()=>{
-              try{
-                const updatedFiles = await network.state.data.files || {}
-                const sandbox = await Sandbox.connect(sandboxId)
-                for(const file of files){
-                  await sandbox.files.write(file.path, file.content)
-                  updatedFiles[file.path] = file.content
+          handler: async ({ files }, { step, network }) => {
+            const newFiles = await step?.run(
+              "createOrUpdateFiles",
+              async () => {
+                try {
+                  const updatedFiles = (await network.state.data.files) || {};
+                  const sandbox = await Sandbox.connect(sandboxId);
+                  for (const file of files) {
+                    await sandbox.files.write(file.path, file.content);
+                    updatedFiles[file.path] = file.content;
+                  }
+                  return updatedFiles;
+                } catch (error) {
+                  return "Error: " + error;
                 }
-                return updatedFiles
-              }catch(error){
-                return "Error: " + error
               }
-            })
-            if(typeof newFiles === "object"){
-              network.state.data.files = newFiles
+            );
+            if (typeof newFiles === "object") {
+              network.state.data.files = newFiles;
             }
-          }
+          },
         }),
-        
+
         //3. Reading Files
         createTool({
           name: "readFiles",
           description: "Read files from the sandbox",
           parameters: z.object({
-            files: z.array(z.string())
+            files: z.array(z.string()),
           }),
-          handler: async({files}, {step})=>{
-            return await step?.run('readFiles', async()=>{
-              try{
+          handler: async ({ files }, { step }) => {
+            return await step?.run("readFiles", async () => {
+              try {
                 const sandbox = await Sandbox.connect(sandboxId);
-                const contents = []
+                const contents = [];
                 for (const file of files) {
                   const content = await sandbox.files.read(file);
-                  contents.push({path: file, content})
+                  contents.push({ path: file, content });
                 }
-                return JSON.stringify(contents)
-              }catch(error){
-                return "Error: " + error
+                return JSON.stringify(contents);
+              } catch (error) {
+                return "Error: " + error;
               }
-            })
-            
+            });
+          },
+        }),
+      ],
+      lifecycle: {
+        onResponse: async ({ result, network }) => {
+          const lastAssistantMessageText =
+            lastAssistantTextMessageContent(result);
+          if (lastAssistantMessageText && network) {
+            if (lastAssistantMessageText.includes("<task_summary>")) {
+              network.state.data.summary = lastAssistantMessageText;
+            }
+          }
+          return result;
+        },
+      },
+    });
+
+    const network = createNetwork({
+      name: "coding-agent-network",
+      agents: [codeAgent],
+      maxIter: 15,
+      router: async ({ network }) => {
+        const summary = network.state.data.summary;
+        if (summary) {
+          return;
+        }
+        return codeAgent;
+      },
+    });
+
+    const result = await network.run(event.data.value);
+    const isError =
+      !result.state.data.summary ||
+      Object.keys(result.state.data.files || {}).length === 0;
+
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await Sandbox.connect(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    });
+
+    await step.run('save-result', async()=>{
+      if(isError){
+        return await prisma.message.create({
+          data: {
+            projectId: event.data.projectId,
+            content: "Something went wrong",
+            role: MessageRole.ASSISTANT,
+            type: MessageType.ERROR
           }
         })
-      ],
-    });
+      }
+      return await prisma.message.create({
+        data: {
+          projectId: event.data.projectId,
+          content: result.state.data.summary,
+          role: MessageRole.ASSISTANT,
+          type: MessageType.RESULT,
+          fragments: {
+            create: {
+              sandboxUrl: sandboxUrl,
+              title: "Untitled",
+              files: result.state.data.files
+            }
+          }
+        },
+      });
+    })
   }
 );
 
-export const functions = [helloWord];
+export const functions = [codeAgentFunction];
---
+++ backend/src/routes/project/index.ts
@@ -0,0 +1,118 @@
+import { getAuth } from '@clerk/express';
+import express from 'express'
+import { prisma } from '../../lib/prisma';
+import { generateSlug } from 'random-word-slugs';
+import {inngest} from '../../inngest/functions'
+import { success } from 'zod';
+export const projectRoute: express.Router = express.Router()
+
+projectRoute.post('/create-project', async (req, res) => {
+  try{
+    const {userId} = getAuth(req)
+    const {content} = req.body
+    if(!userId){
+      throw new Error('User unautorized')
+    }
+    const newProject = await prisma.project.create({
+      data: {
+        title: generateSlug(4, {format: 'kebab'}),
+        userId: userId,
+        messages: {
+          create:{
+            content: content,
+            role: 'USER',
+            type: 'RESULT'
+          }
+        }
+
+      }
+    })
+
+    await inngest.send({
+      name: "code-agent",
+      data: {
+        value: content,
+        projectId: newProject.id
+      }
+    });
+
+    return res.status(200).json({
+      message: "Project Created Success",
+      newProject
+    })
+  }
+  catch(error){
+    console.log("SOmething went wrong while creating Project", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error"
+    })
+  }
+})  
+
+projectRoute.get('get-all-projects', async(req, res)=>{
+  try{
+    const {userId} = getAuth(req)
+    if(!userId) throw new Error("User unautorized");
+    
+    const projects = await prisma.project.findMany({
+      where:{
+        userId: userId
+      },
+      orderBy: {
+        createdAt: "desc"
+      }
+    })
+    return res.status(200).json({
+      success: true,
+      projects
+    })
+  }catch(error){
+    console.log(`Something went wrong while getting all projects`, error)
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error"
+    })
+  }
+})
+
+projectRoute.get("/:projectId", async (req, res) => {
+  try {
+    const { userId } = getAuth(req);
+    if (!userId) throw new Error("User unautorized");
+    const {projectId} = req.params
+
+    if (!projectId) {
+      return res.status(404).json({
+        success: false,
+        message: "Project id not defined"
+      })
+    }
+
+    const project = await prisma.project.findUnique({
+      where: {
+        id: projectId,
+        userId: userId,
+      },
+    });
+    
+    if(!project){
+      return res.status(404).json({
+        success: false,
+        message: `Project with id: ${projectId} not found`,
+      });
+    }
+    
+    return res.status(200).json({
+      success: true,
+      project,
+    });
+  } catch (error) {
+    console.log(`Something went wrong while getting the project`, error);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+});
+
